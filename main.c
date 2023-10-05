e9cd9a07f6ebe07a49d4753581ce776e024e146d
Bob Bjorklund <bjorklr1@tcnj.edu>
2023-10-05 13:20:30 -0400

    made first option use 2 threads

--- main.c
+++ main.c
@@ -3,14 +3,125 @@
 #include <sys/types.h>
 #include <omp.h>
 #include <pthread.h>
-
-int main(int argc, char** argv) {
-    char* solved = "YES";
-    int option;
-    int s[9][9];
+char* solved = "YES";
+int s[9][9];
+void* colsRows(void* param) {
     int c[9] = {0,0,0,0,0,0,0,0,0};
     int c2[9] = {0,0,0,0,0,0,0,0,0};
-    int c3[9] = {0,0,0,0,0,0,0,0,0}; 
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++) {
+            c[s[i][j]-1]++;//= c[s[i][j]-1]+1;
+            c2[s[j][i]-1]++;//= c2[s[j][i]-1]+1;
+        }
+        for (int j = 0; j < 9; j++){
+            if (c[j] != i+1 || c2[j] != i+1) {
+                solved = "NO";
+            }
+        }
+    }
+    for (int i = 0;i<9;i++) {
+        if (c[i] != 9 || c2[i] !=9) {
+            solved = "NO";
+        }
+    }
+}
+void* squares(void* param) {
+    int c3[9] = {0,0,0,0,0,0,0,0,0};
+// squares implementation
+    for (int i = 0; i < 3; i ++) {
+        for (int j = 0; j < 3; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 1) {
+            solved = "NO";
+        }
+    }
+    for (int i = 0; i < 3; i ++) {
+        for (int j = 3; j < 6; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 2) {
+            solved = "NO";
+        }
+    }
+    for (int i = 0; i < 3; i ++) {
+        for (int j = 6; j < 9; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 3) {
+            solved = "NO";
+        }
+    }
+    for (int i = 3; i < 6; i ++) {
+        for (int j = 0; j < 3; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 4) {
+            solved = "NO";
+        }
+    }
+    for (int i = 3; i < 6; i ++) {
+        for (int j = 3; j < 6; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 5) {
+            solved = "NO";
+        }
+    }
+    for (int i = 3; i < 6; i ++) {
+        for (int j = 6; j < 9; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 6) {
+            solved = "NO";
+        }
+    }
+    for (int i = 6; i < 9; i ++) {
+        for (int j = 0; j < 3; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 7) {
+            solved = "NO";
+        }
+    }
+    for (int i = 6; i < 9; i ++) {
+        for (int j = 3; j < 6; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 8) {
+            solved = "NO";
+        }
+    }
+    for (int i = 6; i < 9; i ++) {
+        for (int j = 6; j < 9; j++){
+            c3[s[i][j]-1]++;
+        }
+    }
+    for (int j = 0; j < 9; j++) {
+        if (c3[j] != 9) {
+            solved = "NO";
+        }
+    }
+}
+int main(int argc, char** argv) {
+    
+    int option;
     int failFlag = 0;
     if (argc==2) {
         option = atoi(argv[1]);
@@ -25,119 +136,20 @@ int main(int argc, char** argv) {
         fscanf(fp,"%d %d %d %d %d %d %d %d %d",&s[i][0],&s[i][1],&s[i][2],&s[i][3],&s[i][4],&s[i][5],&s[i][6],&s[i][7],&s[i][8]);
         printf("%d %d %d %d %d %d %d %d %d\n",s[i][0],s[i][1],s[i][2],s[i][3],s[i][4],s[i][5],s[i][6],s[i][7],s[i][8]);
     }
+
     if (option == 1) {
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 9; j++) {
-                c[s[i][j]-1]++;//= c[s[i][j]-1]+1;
-                c2[s[j][i]-1]++;//= c2[s[j][i]-1]+1;
-            }
-            for (int j = 0; j < 9; j++){
-                if (c[j] != i+1 || c2[j] != i+1) {
-                    solved = "NO";
-                }
-            }
-        }
-        // squares implementation
-        for (int i = 0; i < 3; i ++) {
-            for (int j = 0; j < 3; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 1) {
-                solved = "NO";
-            }
-        }
-        for (int i = 0; i < 3; i ++) {
-            for (int j = 3; j < 6; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 2) {
-                solved = "NO";
-            }
-        }
-        for (int i = 0; i < 3; i ++) {
-            for (int j = 6; j < 9; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 3) {
-                solved = "NO";
-            }
-        }
-        for (int i = 3; i < 6; i ++) {
-            for (int j = 0; j < 3; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 4) {
-                solved = "NO";
-            }
-        }
-        for (int i = 3; i < 6; i ++) {
-            for (int j = 3; j < 6; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 5) {
-                solved = "NO";
-            }
-        }
-        for (int i = 3; i < 6; i ++) {
-            for (int j = 6; j < 9; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 6) {
-                solved = "NO";
-            }
-        }
-        for (int i = 6; i < 9; i ++) {
-            for (int j = 0; j < 3; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 7) {
-                solved = "NO";
-            }
-        }
-        for (int i = 6; i < 9; i ++) {
-            for (int j = 3; j < 6; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 8) {
-                solved = "NO";
-            }
-        }
-        for (int i = 6; i < 9; i ++) {
-            for (int j = 6; j < 9; j++){
-                c3[s[i][j]-1]++;
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (c3[j] != 9) {
-                solved = "NO";
-            }
-        }
+        pthread_t pt1, pt2;
+        pthread_create(&pt1,NULL, colsRows,NULL);
+        pthread_create(&pt2,NULL,squares,NULL);
+        pthread_join(pt1, NULL);
+        pthread_join(pt2,NULL);
+        
     } else if (option == 2) {
 
     } else if (option == 3) {
 
     }
-    for (int i = 0;i<9;i++) {
-        if (c[i] != 9 || c2[i] !=9) {
-            solved = "NO";
-        }
-    }
+
     //printf("%d %d %d %d %d %d %d %d %d\n",c3[0],c3[1],c3[2],c3[3],c3[4],c3[5],c3[6],c3[7],c3[8]);
     // }
     // printf("that's a good solution\n");
